abf95ffab267e4a0406a1d44f88d25aae0dd7885
Kobe <kobezhe@gmail.com>
2024-04-17 16:07:08 -0400

    adding home test

--- app/src/androidTest/java/com/example/myrecipes/UI/LoginTest.kt
+++ app/src/androidTest/java/com/example/myrecipes/UI/LoginTest.kt
@@ -1,6 +1,7 @@
 package com.example.myrecipes.UI
 
 import androidx.compose.material.Text
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
@@ -9,44 +10,28 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.myrecipes.modelview.LoginViewModel
 import com.example.myrecipes.modelview.SavedRecipesViewModel
 import com.example.myrecipes.view.UI.Login
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.Before
-import org.mockito.Mock
+import org.mockito.Mockito.anyString
 import org.mockito.Mockito.mock
-import org.mockito.MockitoAnnotations
+import org.mockito.Mockito.`when`
 
 @RunWith(AndroidJUnit4::class)
 class LoginTest {
     @get:Rule
     val composeTestRule = createComposeRule()
 
-//    @Mock
-//    private lateinit var navController: NavHostController 
-
-    @Mock
-    private lateinit var savedRecipesViewModel: SavedRecipesViewModel
-
-    @Mock
-    private lateinit var loginViewModel: LoginViewModel
+    private var navController: NavHostController = mock(NavHostController::class.java)
 
     @Before
     fun setUp() {
-        MockitoAnnotations.openMocks(this)
         composeTestRule.setContent {
-//            Login(
-//                navController = navController,
-//                savedRecipesViewModel = savedRecipesViewModel,
-//                viewModel = loginViewModel
-//            )
-            Text(text = "hey")
+
         }
     }
 
-    @Test
-    fun testLoginScreen() {
-        composeTestRule.onNodeWithText("Login").assertIsDisplayed()
-        composeTestRule.onNodeWithText("Back").assertIsDisplayed()
-    }
 }
